8a3f750defd8344840d11debdb2a8c09866a70f6
Hugo EXTRAT <extrat.h@gmail.com>
2026-03-07 09:47:26 +0100

    chore: add example

---
+++ example/src/App.tsx
@@ -0,0 +1,38 @@
+import { Button, StyleSheet, View } from "react-native";
+import RateApp, { AndroidMarket } from "react-native-rate-app";
+
+export default function App() {
+  const onPressRate = async () => {
+    const result = await RateApp.requestReview({
+      androidMarket: AndroidMarket.SAMSUNG,
+      androidPackageName: "com.instagram.android",
+    }).catch((err) => console.log(err));
+    console.log("rate", result);
+  };
+
+  const onPressOpenStoreForReview = async () => {
+    const result = await RateApp.openStoreForReview({
+      iOSAppId: "389801252",
+      androidPackageName: "com.instagram.android",
+      androidMarket: AndroidMarket.GOOGLE,
+    }).catch((err) => console.log(err));
+    console.log("store listing", result);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title={"Rate"} onPress={onPressRate} />
+      <Button title={"Open Store Review"} onPress={onPressOpenStoreForReview} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+    backgroundColor: "white",
+  },
+});
